ec6a1e48fb631622f0f7b0b61fbf13f9fbcc6edc
Sophia Blazizza <sblazizza@gmail.com>
2024-01-06 03:20:20 -0300

    adicionando lista de contratos, construtores e metodos na classe Worker

--- SalaryOfEachWorker/Entities/Worker.cs
+++ SalaryOfEachWorker/Entities/Worker.cs
@@ -1,4 +1,6 @@
-﻿using SalaryOfEachWorker.Entities.Enums;
+﻿using System.Collections.Generic;
+using SalaryOfEachWorker.Entities.Enums;
+
 
 namespace SalaryOfEachWorker.Entities
 {
@@ -8,6 +10,42 @@ namespace SalaryOfEachWorker.Entities
         public WorkerLevel Level { get; set; }
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
+        public List<HourContract> Contracts { get; set; } = new List<HourContract>();
+
+        public Worker()
+        {
+        }
+
+        public Worker(string name, WorkerLevel level, double baseSalary, Department department)
+        {
+            Name = name;
+            Level = level;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
+
+        public void AddContract(HourContract contract)
+        {
+            Contracts.Add(contract);
+        }
+
+        public void RemoveContract(HourContract contract)
+        {
+            Contracts.Remove(contract);
+        }
+
+        public double Income(int year, int month) 
+        {
+            double sum = BaseSalary;
+            foreach (HourContract contract in Contracts) 
+            { 
+                if (contract.Date.Year == year && contract.Date.Month == month)
+                {
+                    sum += contract.TotalValue();
+                }
+            }
+            return sum;
+        }
 
     }
 }
