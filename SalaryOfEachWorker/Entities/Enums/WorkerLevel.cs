f2f69ba045c261adb92a8c2a65d4660b93633a60
Sophia Blazizza <sblazizza@gmail.com>
2024-01-06 02:53:44 -0300

    iniciando o projeto adicionando classes e enum

---
+++ SalaryOfEachWorker/Entities/Enums/WorkerLevel.cs
@@ -0,0 +1,9 @@
+﻿namespace SalaryOfEachWorker.Entities.Enums
+{
+    enum WorkerLevel : int
+    {
+        Junior = 0,
+        MidLevel = 1,
+        Senior = 2
+    }
+}
---
+++ SalaryOfEachWorker/Entities/Worker.cs
@@ -0,0 +1,13 @@
+﻿using SalaryOfEachWorker.Entities.Enums;
+
+namespace SalaryOfEachWorker.Entities
+{
+    class Worker
+    {
+        public string Name { get; set; }
+        public WorkerLevel Level { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }
+
+    }
+}
